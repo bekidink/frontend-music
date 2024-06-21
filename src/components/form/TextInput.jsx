b31c0329f8a32d238ed44d3af64f3172fe2979fd
bereketdinku <bereketdinku80@gmail.com>
2024-06-21 04:16:15 -0700

    copypastvalid

--- src/components/form/TextInput.jsx
+++ src/components/form/TextInput.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function TextInput({name,setName,placeholder}) {
+  const handlePaste = (event) => {
+    event.preventDefault(); // Prevent default paste behavior
+    // Optionally, you can show a message or perform some action to handle the paste attempt
+  };
   return (
     <input
         className="w-full my-3 p-3 rounded-md text-base font-semibold text-textColor outline-none border shadow-sm border-gray-300 bg-transparent"
@@ -8,6 +12,7 @@ export default function TextInput({name,setName,placeholder}) {
         placeholder={placeholder}
         value={name}
         onChange={(e) => setName(e.target.value)}
+        onPaste={handlePaste}
       />
   )
 }
